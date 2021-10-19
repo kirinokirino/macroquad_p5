a16a8921c46d9358a3957e627f551ff9ae4f41ef
kirinokirino <kirinokirino@lunariko.com>
2021-10-19 18:53:16 +0300

    Replaces env_logger for simple_logger.

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,12 @@
-#[macro_use] extern crate log;
-extern crate env_logger;
+#[macro_use]
+extern crate log;
+use simple_logger::SimpleLogger;
 
 use log::Level;
 
 fn main() {
-    env_logger::init();
-    println!("Hello, world!");
+    SimpleLogger::new().init().unwrap();
+
     debug!("this is a debug {}", "message");
     if log_enabled!(Level::Info) {
         let x = fastrand::i32(1..20); // expensive computation
