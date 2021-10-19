d27dcc7502b44042a9cdfb701674d968aceebc62
kirinokirino <kirinokirino@lunariko.com>
2021-10-19 18:25:49 +0300

    Adds logging.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+#[macro_use] extern crate log;
+extern crate env_logger;
+
+use log::Level;
+
 fn main() {
+    env_logger::init();
     println!("Hello, world!");
+    debug!("this is a debug {}", "message");
+    if log_enabled!(Level::Info) {
+        let x = 3 * 4; // expensive computation
+        info!("the answer was: {}", x);
+    }
 }
