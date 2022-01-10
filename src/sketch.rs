141f40164ae2ee2427d8c80ea2a8fe7f23efa3ee
kirinokirino <kirinokirino@lunariko.com>
2022-01-10 21:56:36 +0300

    Refactored the basic bars sketch.

--- src/sketch.rs
+++ src/sketch.rs
@@ -1,19 +1,51 @@
 use crate::*;
 
 const NUMBER_OF_BARS: usize = 200;
-static mut BARS: [f32; NUMBER_OF_BARS] = [20.0; NUMBER_OF_BARS];
-static mut COLORS: [(u8, u8, u8, u8); NUMBER_OF_BARS] =
-    [(255_u8, 255_u8, 0_u8, 255_u8); NUMBER_OF_BARS];
+static mut BARS_: Vec<Bar> = vec![];
+struct Bar {
+    color: Color,
+    width: f32,
+    height: f32,
+    x: f32,
+}
+
+impl Bar {
+    const fn new(color: Color, width: f32, x: f32) -> Self {
+        Self {
+            color,
+            width,
+            height: 1.0,
+            x,
+        }
+    }
+
+    fn draw(&self) {
+        draw_rectangle(
+            self.x,
+            -screen_height(),
+            self.width,
+            self.height,
+            self.color,
+        );
+    }
+}
 
 pub fn setup() {
     unsafe {
-        for color in COLORS.iter_mut() {
-            *color = (
+        BARS_.reserve(NUMBER_OF_BARS);
+        let width = screen_width() * 2.0 / NUMBER_OF_BARS as f32;
+        for i in 0..NUMBER_OF_BARS {
+            let color = Color::from_rgba(
                 rand::gen_range(0_u8, 100_u8),
                 rand::gen_range(100_u8, 200_u8),
                 rand::gen_range(100_u8, 200_u8),
                 255_u8,
             );
+            BARS_.push(Bar::new(
+                color,
+                width - 2.0,
+                width.mul_add(i as f32, -screen_width()),
+            ));
         }
     }
     info!("Setup!");
@@ -21,18 +53,11 @@ pub fn setup() {
 
 pub fn draw(_delta: f64) {
     unsafe {
-        for (i, &bar) in BARS.iter().enumerate() {
-            let width = screen_width() * 2.0 / NUMBER_OF_BARS as f32;
-            draw_rectangle(
-                ((width + 2.0) * i as f32) - screen_width(),
-                -screen_height(),
-                width,
-                bar,
-                color_u8!(COLORS[i].0, COLORS[i].1, COLORS[i].2, COLORS[i].3),
-            );
+        for bar in &BARS_ {
+            bar.draw();
         }
-        draw_circle(0.0, 0.0, 2.0, colors::BEIGE);
-        BARS[rand::gen_range(0, 200)] += 5.0;
+
+        BARS_[rand::gen_range(0, NUMBER_OF_BARS)].height += 5.0;
     }
     draw_ui();
 }
